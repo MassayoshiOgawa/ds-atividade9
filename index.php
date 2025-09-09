f71a4555007e696b93b2851c6ac85224150b912c
MassayoshiOgawa <psnnapoleandro@gmail.com>
2025-09-09 08:57:41 -0300

    commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1 class="textheader">Biblioteca</h1>
+    </header>
+
+    <main>
+        <form method="post">
+            <h1>Login</h1>
+            <label>Username:</label>
+             <input type="text" name="" id="">
+            <label>Password:</label>
+            <input type="password" name="" id="">
+        </form>
+    </main>
+</body>
+</html>
